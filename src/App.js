d04e43f6fb0738eabb8ae5b6d16974f38d524300
Sachin <ssolanki5841@gmail.com>
2024-11-16 17:34:23 +0530

    "App.js changes"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import FormComponent from './FormComponent';
+import SearchBar from './SearchBar';
+import LocalStorage from './LocalStorage';
 
-function App() {
+export default function MyApp() {
+  let onChange = (e) => {
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* <FormComponent/> */}
+      {/* <SearchBar /> */}
+      <LocalStorage/>
     </div>
   );
 }
 
-export default App;
